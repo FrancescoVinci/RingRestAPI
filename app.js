36a49ac9d23d025f3af4b87efcbf382de44b7051
Davide Pizzolato <pizzolato.davide00@gmail.com>
2022-04-04 18:45:22 +0200

    Add .env

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+require('dotenv').config()
+
 const express = require('express');
 const fs = require('fs');
 const https = require('https');
+const http = require('http');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const apiRoute = require('./routes/api');
 const cors = require('cors');
 
+
 app.use(function (req, res, next){
-	res.setHeader('Access-Control-Allow-Origin', 'https://www.dsi.unive.it'); 
+	res.setHeader('Access-Control-Allow-Origin', 'https://www.dsi.unive.it');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 	res.setHeader('Access-Control-Allow-Credentials', true);
@@ -28,9 +32,17 @@ app.use(bodyParser.urlencoded({
 
 app.use('/api', apiRoute);
 
-var credentials = {
-	key: fs.readFileSync('/etc/letsencrypt/live/ring.dais.unive.it/privkey.pem','utf8'),
-	cert: fs.readFileSync('/etc/letsencrypt/live/ring.dais.unive.it/fullchain.pem','utf8')
-};
+let server;
+if(process.env.HTTPS && process.env.HTTPS.toLowerCase() === 'true') {
+	let credentials = {
+		key: fs.readFileSync(process.env.HTTPS_KEY, 'utf8'),
+		cert: fs.readFileSync(process.env.HTTPS_CERT, 'utf8')
+	};
 
-https.createServer(credentials, app).listen(8002);
+	server = https.createServer(credentials, app);
+}
+else {
+	server = http.createServer(app);
+}
+
+server.listen(process.env.PORT || 8000, '0.0.0.0')
